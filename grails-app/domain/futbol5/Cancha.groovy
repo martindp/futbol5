1e7ac5459004d959b794d3042f01b057842a7fd2
martindp <martinmaildp@hotmail.com>
2015-04-26 19:55:21 -0300

    Pruebas de constrains de Cancha, creacion de Usuario y Reserva y sus respectivos controladores

--- grails-app/domain/futbol5/Cancha.groovy
+++ grails-app/domain/futbol5/Cancha.groovy
@@ -2,6 +2,15 @@ package futbol5
 
 class Cancha {
 
+    String nombre
+    String direccion
+    String telefono
+    String precio
+
     static constraints = {
+    nombre(nullable:false, blank:false, unique:true)
+    direccion(nullable:false, blank:false)
+    telefono(nullable:false, blank:false)
+    precio(nullable:false, blank:false)
     }
 }
--- test/unit/futbol5/CanchaSpec.groovy
+++ test/unit/futbol5/CanchaSpec.groovy
@@ -9,11 +9,39 @@ import spock.lang.Specification
 @TestFor(Cancha)
 class CanchaSpec extends Specification {
 
-    def setup() {
-    }
+    def setup() {}
 
     def cleanup() {
     }
+
+    void 'Ningun dato de la cancha puede ser nulo'() {
+
+    when: 'Cuando el nombre es nulo'
+    def c= new Cancha(direccion:'Direccion1', telefono:'Telefono1', precio:'Precio1')
+
+    then: !c.validate()
+
+    when: 'Cuando la direccion es nula'
+        c= new Cancha(nombre:'Cancha1', telefono:'Telefono1', precio:'Precio1')
+
+    then: !c.validate()
+
+    when: 'Cuando el telefono es nulo'
+    c= new Cancha(nombre:'Cancha1',  direccion:'Direccion1', precio:'Precio1')
+
+    then: !c.validate()
+
+    when: 'Cuando el precio es nulo'
+    c= new Cancha(nombre:'Cancha1',  direccion:'Direccion1', telefono:'Telefono1')
+
+    then: !c.validate()
+
+    when: 'Cuando nada es nulo'
+    c= new Cancha(nombre:'Cancha1', direccion:'Direccion1', telefono:'Telefono1', precio:'Precio1')
+
+    then: c.validate()
+    }
+
 
 
 }
