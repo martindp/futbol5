cb2402fdaa25be31141d204d7465882103e6715f
yaninaespinosa <yanina.espinosaprette@gmail.com>
2015-06-08 19:42:28 -0300

    mapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,12 +2,6 @@ class UrlMappings {
 
 	static mappings = {
 
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
-
         "/cancha"(controller:"cancha", action:"index", method:"GET")
 
         "/"(controller:"home", action:"index")
