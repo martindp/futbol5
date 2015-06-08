ce298b09f59cbe3018fad573b6cf4e7fbabd78bd
yaninaespinosa <yanina.espinosaprette@gmail.com>
2015-06-08 18:12:20 -0300

    mapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
 
         "/cancha/index"(controller: 'cancha', action: 'index')
 
