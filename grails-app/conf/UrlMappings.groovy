d916485da6294713d8dc0d89c16340b1f5d1b081
martindp <martinmaildp@hotmail.com>
2015-06-09 21:21:03 -0300

    urlmappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,8 +1,15 @@
 class UrlMappings {
 
 	static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
 
-        "/api/canchas"(resources: 'cancha')
+		"/api/canchas"(resources: 'cancha')
 
+        "/"(view:"/index")
+        "500"(view:'/error')
 	}
 }
